450edd50a858fa7039fb649dc2337babb5b1a92e
tirthraj2003 <tirthrajtrivedi2003@gmail.com>
2024-11-28 15:36:55 +0530

    Resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,50 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Row } from 'react-bootstrap'
-import Button from 'react-bootstrap/Button'
-import Particle from '../Particle'
-import pdf from '../../Assets/tirthraj.pdf'
-import { AiOutlineDownload } from 'react-icons/ai'
-import { Document, Page, pdfjs } from 'react-pdf'
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
-
-// const resumeLink = 'https://github.com/tirthraj2003/My-Resume.git';
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/Resume.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200)
+  const [width, setWidth] = useState(1200);
 
   useEffect(() => {
-    setWidth(window.innerWidth)
-  }, [])
+    setWidth(window.innerWidth);
+  }, []);
 
   return (
     <div>
-      <Container fluid className='resume-section'>
+      <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
-            variant='primary'
+            variant="primary"
             href={pdf}
-            target='_blank'
-            style={{ maxWidth: '250px' }}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
         </Row>
 
-        <Row className='resume'>
-          <Document file={pdf} className='d-flex justify-content-center'>
+        <Row className="resume">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
-            variant='primary'
+            variant="primary"
             href={pdf}
-            target='_blank'
-            style={{ maxWidth: '250px' }}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
             &nbsp;Download CV
@@ -52,7 +50,7 @@ function ResumeNew() {
         </Row>
       </Container>
     </div>
-  )
+  );
 }
 
-export default ResumeNew
+export default ResumeNew;
